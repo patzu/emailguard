7645ce0a6ec2b8b9862c73753ab6829188151b97
Davoud Badamchi <Davoud.badamchi@gmail.com>
2025-01-18 11:14:19 +0330

    Initial commit.

---
+++ src/main/java/com/example/emailguard/EmailguardApplication.java
@@ -0,0 +1,13 @@
+package com.example.emailguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailguardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailguardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/emailguard/EmailguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emailguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailguardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
